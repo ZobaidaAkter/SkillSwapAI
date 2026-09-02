3e15e3229b63b8ef8e423d3f1bd08b91a6c5ed2c
ZobaidaAkter <115462102+ZobaidaAkter@users.noreply.github.com>
2026-09-02 19:05:54 +0600

    Delete routes directory

--- routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getProfile } = require("../controllers/profileController");
-
-// Get user profile
-router.get("/:user_id", getProfile);
-
-module.exports = router;
